76ececb85501fc873a2b2ecbd17410c2eafada10
Richard Pearson <richard.pearson@postgrad.manchester.ac.uk>
2008-10-21 14:11:23 +0000

    Added a minimal NAMESPACE Removed .First.lib function Bumped version number to 1.7.5
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/puma@34786 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,18 @@
-.First.lib <- function(lib, pkgname, where) {
-  ## load the compiled code
-  library.dynam("puma", pkgname, lib)
-
-  if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
-        && .Platform$GUI ==  "Rgui"){
-        addVigs2WinMenu("puma")
-    }
-
-  cat(paste("\nWelcome to puma version", packageDescription('puma')$Version,"\n\n"))
-
-  cat("puma is free for research purposes only. For more details, type\n")
-  cat("license.puma(). Type citation('puma') for details on how to cite\n")
-  cat("puma in publications.\n")
-}
+#.First.lib <- function(lib, pkgname, where) {
+#  ## load the compiled code
+#  library.dynam("puma", pkgname, lib)
+#
+#  if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
+#        && .Platform$GUI ==  "Rgui"){
+#        addVigs2WinMenu("puma")
+#    }
+#
+#  cat(paste("\nWelcome to puma version", packageDescription('puma')$Version,"\n\n"))
+#
+#  cat("puma is free for research purposes only. For more details, type\n")
+#  cat("license.puma(). Type citation('puma') for details on how to cite\n")
+#  cat("puma in publications.\n")
+#}
 
 license.puma <- function(){
 
